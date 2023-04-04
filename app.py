cd387b0f082f62f1f0c2c8786f6423b219ab90d3
Oscar Caicedo <oscarccd17@gmail.com>
2023-04-03 20:30:27 -0500

    parsing text and printing different tags

--- app.py
+++ app.py
@@ -4,4 +4,12 @@ from bs4 import BeautifulSoup
 url = 'https://genius.com/albums/Tyler-the-creator/Call-me-if-you-get-lost-the-estate-sale'
 html = requests.get(url)
 
-print(html)
+# print(html)
+
+soup = BeautifulSoup(html.content, 'html.parser')
+
+lst = soup.find_all("h3", class_='chart_row-content-title')
+
+print(lst)
+
+# print(soup.prettify())
