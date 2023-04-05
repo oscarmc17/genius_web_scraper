01f464b169ddc4c5541ef8a9a2b94b96612b1912
Oscar Caicedo <oscarccd17@gmail.com>
2023-04-04 21:17:44 -0500

    refactoring 3 main vars with .text attribute and print statement created with an f-string

--- app.py
+++ app.py
@@ -10,14 +10,19 @@ soup = BeautifulSoup(html.content, 'html.parser')
 
 tracks = soup.find_all('h3', {'class': 'chart_row-content-title'})
 album_title = soup.find('h1', {
-                        'class': 'header_with_cover_art-primary_info-title header_with_cover_art-primary_info-title--white'})
+                        'class': 'header_with_cover_art-primary_info-title header_with_cover_art-primary_info-title--white'}).text
 artist_name = soup.find(
-    'a', {'class': 'header_with_cover_art-primary_info-primary_artist'})
+    'a', {'class': 'header_with_cover_art-primary_info-primary_artist'}).text
+release_date = soup.find('div', {'class': 'metadata_unit'}).text
 
 count = 0
 
-print('\nArtist: {}'.format(artist_name.text))
-print("Album Name: {}\n".format(album_title.text))
+# print('\nArtist: {}'.format(artist_name.text))
+# print("Album Name: {}".format(album_title.text))
+# print(release_date)
+
+print(f'\nArtist: {artist_name}\nAlbum Name: {album_title}\n{release_date}\n')
+
 for song in tracks:
     count += 1
     song_title = song.text.strip().replace('Lyrics', '')
