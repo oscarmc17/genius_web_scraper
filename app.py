106b91be10b794a18a60670251b0711914f1b5e4
Oscar Caicedo <oscarccd17@gmail.com>
2023-04-04 21:04:44 -0500

    Artist and Album name displayed

--- app.py
+++ app.py
@@ -8,12 +8,23 @@ html = requests.get(url)
 
 soup = BeautifulSoup(html.content, 'html.parser')
 
-tracks = soup.find_all(class_='chart_row-content-title')
+tracks = soup.find_all('h3', {'class': 'chart_row-content-title'})
+album_title = soup.find('h1', {
+                        'class': 'header_with_cover_art-primary_info-title header_with_cover_art-primary_info-title--white'})
+artist_name = soup.find(
+    'a', {'class': 'header_with_cover_art-primary_info-primary_artist'})
 
 count = 0
-new = []
 
+print('\nArtist: {}'.format(artist_name.text))
+print("Album Name: {}\n".format(album_title.text))
 for song in tracks:
-    # new.append(song)
     count += 1
-    print("{}. {}".format(count, song.text.strip()))
+    song_title = song.text.strip().replace('Lyrics', '')
+
+    print("{}. {}".format(count, song_title))
+
+# for i, title in enumerate(tracks, start=1):
+#     # Remove the "Lyrics" text from the track title
+#     title_text = title.text.strip().replace(' Lyrics', '')
+#     print(f"{i}. {title_text}")
